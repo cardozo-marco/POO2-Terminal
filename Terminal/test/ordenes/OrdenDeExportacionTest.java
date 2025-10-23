765a8c065f5ec87d92d476fc5038311962a7706c
Gabriel Gallo <gfgallo12@gmail.com>
2025-10-23 18:47:32 -0300

    orden con test verdes

---
+++ Terminal/test/ordenes/OrdenDeExportacionTest.java
@@ -0,0 +1,91 @@
+package ordenes;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.*;
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+
+import carga.*;
+import paraPruebas.*;
+import servicios.*;
+
+public class OrdenDeExportacionTest {
+	public OrdenDeExportacion ordenDeExportacion;
+	
+	public Shipper mockShipper;
+	public Container mockContainer;
+	public Viaje mockViaje;
+	public Camion mockCamion;
+	public Conductor mockConductor;
+	public TerminalPortuaria mockTerminalPortuaria;
+	
+	public ServicioLavado mockLavado;
+	public ServicioElectricidad mockElectricidad;
+	
+	public LocalDateTime turnoFicticio;
+	
+	@BeforeEach
+	public void setUp() {
+		mockShipper = mock(Shipper.class);
+		mockContainer = mock(Container.class);
+		mockViaje = mock(Viaje.class);
+		mockCamion = mock(Camion.class);
+		mockConductor = mock(Conductor.class);
+		mockTerminalPortuaria = mock(TerminalPortuaria.class);
+		
+		mockLavado = mock(ServicioLavado.class);
+		mockElectricidad = mock(ServicioElectricidad.class);
+		
+		turnoFicticio = LocalDateTime.of(2025, 10, 23, 18, 0);
+		
+		ordenDeExportacion = new OrdenDeExportacion(mockShipper, mockContainer, mockViaje, mockCamion, mockConductor, mockTerminalPortuaria);
+	}
+	
+	@Test
+	public void tunoAsignadoTest() {
+		ordenDeExportacion.setTurnoAsignado(turnoFicticio);
+		assertEquals(turnoFicticio, ordenDeExportacion.getTurnoAsignado());
+	}
+	
+	@Test
+	public void agregarServiciosTest() {
+		//verifico que no tengo servicios
+		assertTrue(ordenDeExportacion.serviciosContratados().isEmpty());
+		
+		ordenDeExportacion.agregarServicio(mockElectricidad);
+		ordenDeExportacion.agregarServicio(mockLavado);
+		
+		List<Servicio> serviciosEsperados = Arrays.asList(mockElectricidad, mockLavado);
+		
+		assertEquals(2, ordenDeExportacion.serviciosContratados().size());
+		assertEquals(serviciosEsperados, ordenDeExportacion.serviciosContratados());
+	}
+	
+	@Test
+	public void costoTotalTest() {
+		assertEquals(0.0, ordenDeExportacion.calcularCostoTotalServicios());
+		
+		when(mockElectricidad.calcularCosto(any(Orden.class))).thenReturn(20.0);
+		when(mockLavado.calcularCosto(any(Orden.class))).thenReturn(10.0);
+		
+		ordenDeExportacion.agregarServicio(mockLavado);
+		ordenDeExportacion.agregarServicio(mockElectricidad);
+		
+		assertEquals(30.0, ordenDeExportacion.calcularCostoTotalServicios());
+	}
+	
+	@Test
+	public void cantidadDeDias() {
+		ordenDeExportacion.setTurnoAsignado(LocalDateTime.of(2025, 10, 30, 12, 0));
+		assertEquals(7, ordenDeExportacion.cantidadDeDias());
+	}
+	
+	@Test
+	public void getVolumenTest() {
+		when(mockContainer.getVolumen()).thenReturn(80.0);
+		assertEquals(80.0, ordenDeExportacion.getVolumen());
+	}
+}
---
+++ Terminal/test/ordenes/OrdenDeImportacionTest.java
@@ -0,0 +1,103 @@
+package ordenes;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.*;
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+
+import carga.*;
+import paraPruebas.*;
+import servicios.*;
+
+public class OrdenDeImportacionTest {
+	public OrdenDeImportacion ordenDeImportacion;
+	
+	//mock para crear orden de importacion
+	public Consignee mockConsignee;
+	public Container mockContainer;
+	public Viaje mockViaje;
+	public Camion mockCamion;
+	public Conductor mockConductor;
+	public TerminalPortuaria mockTerminaPortuaria;
+	
+	//mock para servicios
+	public ServicioLavado mockServicioLavado;
+	public ServicioElectricidad mockServicioElectricidad;
+	public ServicioPesado mockServicioPesado;
+	
+	//mock para probar fecha de llegada
+	public LocalDateTime fechaLlegadaFicticia;
+	
+	@BeforeEach
+	public void setUp() {
+		mockConsignee = mock(Consignee.class);
+		mockContainer = mock(Container.class);
+		mockViaje = mock(Viaje.class);
+		mockCamion = mock(Camion.class);
+		mockConductor = mock(Conductor.class);
+		mockTerminaPortuaria = mock(TerminalPortuaria.class);
+		
+		mockServicioLavado = mock(ServicioLavado.class);
+		mockServicioElectricidad = mock(ServicioElectricidad.class);
+		mockServicioPesado = mock(ServicioPesado.class);
+		
+		fechaLlegadaFicticia = LocalDateTime.of(2025, 10, 30, 12, 0);
+		
+		when(mockViaje.getFechaLlegadaA(mockTerminaPortuaria)).thenReturn(fechaLlegadaFicticia);
+		
+		ordenDeImportacion = new OrdenDeImportacion(mockConsignee, mockContainer, mockViaje, mockCamion, mockConductor, mockTerminaPortuaria);
+	}
+	
+	@Test
+	public void FechaDeLlegadaTest() {
+		assertEquals(fechaLlegadaFicticia, ordenDeImportacion.getFechaLlegadaCarga());
+	}
+	
+	@Test
+	public void AgregarServiciosTest() {
+		assertTrue(ordenDeImportacion.serviciosContratados().isEmpty()); //la lista de servicios debe empezar vacia
+		
+		//agrego 3 servicios
+		ordenDeImportacion.agregarServicio(mockServicioLavado);
+		ordenDeImportacion.agregarServicio(mockServicioElectricidad);
+		ordenDeImportacion.agregarServicio(mockServicioPesado);
+		
+		//verifico que se hayan agregados los 3 servicios
+		List<Servicio> serviciosEsperados = Arrays.asList(mockServicioLavado, mockServicioElectricidad, mockServicioPesado);
+		assertEquals(3, ordenDeImportacion.serviciosContratados().size());
+		assertEquals(serviciosEsperados, ordenDeImportacion.serviciosContratados());
+	}
+	
+	@Test
+	public void CostoTotalDeServiciosTest() {
+		//verifico que el costo sea 0 ya que no hay servicios
+		assertEquals(0.0, ordenDeImportacion.calcularCostoTotalServicios());
+		
+		//agrego 3 servicios
+		ordenDeImportacion.agregarServicio(mockServicioLavado);
+		ordenDeImportacion.agregarServicio(mockServicioElectricidad);
+		ordenDeImportacion.agregarServicio(mockServicioPesado);
+		
+		when(mockServicioLavado.calcularCosto(ordenDeImportacion)).thenReturn(10.0);
+		when(mockServicioElectricidad.calcularCosto(ordenDeImportacion)).thenReturn(20.0);
+		when(mockServicioPesado.calcularCosto(ordenDeImportacion)).thenReturn(15.0);
+		
+		//verifico que calcule el costo correcto con los servicios agregados
+		assertEquals(45.0, ordenDeImportacion.calcularCostoTotalServicios());
+	}
+	
+	@Test
+	public void cantidadDiasTest() {
+		assertEquals(7, ordenDeImportacion.cantidadDeDias());
+	}
+	
+	@Test
+	public void getVolumenTest() {
+		when(mockContainer.getVolumen()).thenReturn(80.0);
+		assertEquals(80.0, ordenDeImportacion.getVolumen());
+	}
+}
+
